795672747aa13eb46b88aa42072e24b34ddf82e0
0jc1 <epicdevelopers26@gmail.com>
2025-10-19 21:28:07 -0400

    chore: homebrew support

--- install.sh
+++ install.sh
@@ -9,17 +9,23 @@ log_file="autovod_installation.log"
 # Log all the outputs of the script to the log file
 exec &> >(tee -a "$log_file")
 
-# Check if apt or dnf is installed
+# Check if apt, dnf, or brew is installed
+install_command='install'
 if command -v apt-get &>/dev/null; then
   package_manager='apt-get'
-  install_command='install'
   update_command='update'
   upgrade_command='upgrade'
+  check_install='dpkg -s'
 elif command -v dnf &>/dev/null; then
   package_manager='dnf'
-  install_command='install'
   update_command='check-update'
   upgrade_command='upgrade'
+  check_install='rpm -q'
+elif command -v brew &>/dev/null; then
+  package_manager='brew'
+  update_command='update'
+  upgrade_command='upgrade'
+  check_install='brew list -1 | grep -w'
 else
   printf "${g}[$now] Error: Could not find a supported package manager. Exiting...${c}\n"
   exit 1
@@ -28,23 +34,25 @@ fi
 printf "${g}[$now] Updating and upgrading packages...${c}\n"
 $package_manager -qq $update_command && $package_manager -qq $upgrade_command
 
-printf "${g}[$now] Installing necessary Packages...${c}\n"
+printf "${g}[$now] Installing necessary packages...${c}\n"
 
-# Package names for apt
-apt_packages=(python3-pip tar ffmpeg streamlink)
-# Package names for DNF
-dnf_packages=(python3-pip tar ffmpeg streamlink)
+# Package names for different package managers
+apt_packages=(python3-pip ffmpeg streamlink)
+dnf_packages=(python3-pip ffmpeg streamlink)
+brew_packages=(python@3.12 ffmpeg streamlink) 
 
 # Use the appropriate package array based on the detected package manager
 if [ "$package_manager" = "apt-get" ]; then
   packages=("${apt_packages[@]}")
 elif [ "$package_manager" = "dnf" ]; then
   packages=("${dnf_packages[@]}")
+elif [ "$package_manager" = "brew" ]; then
+  packages=("${brew_packages[@]}")
 fi
 
 # Install the packages
 for package in "${packages[@]}"; do
-  dpkg -s "$package" &>/dev/null
+  $check_install "$package" &>/dev/null
   if [ $? -eq 0 ]; then
     printf "${g}[$now] $package already installed...${c}\n"
   else
